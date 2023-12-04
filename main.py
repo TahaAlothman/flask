e24a47e667f4c65fd294f28dd49adfb8b52e4694
[TahaAlothman] <tahaaborawi@gmail.com>
2023-12-04 12:56:16 +0100

    install db

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False 
+db=SQLAlchemy(app)
+app.app_context().push()
+
+class Message(db.Model):
+    id =db.Column(db.Integer,primary_key=True) 
+    user =db.Column(db.String(200),nullable=True)
+    content =db.Column(db.String(500),nullable=True)
+    created_at =db.Column(db.DateTime(),default =datetime.utcnow)
+
 
 @app.route("/")
 def hello():
